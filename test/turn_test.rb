41d19f13e995df1e102f1f5d440722d372a21486
John Kim <abcdefghijohnk@gmail.com>
2020-08-11 18:04:20 -0600

    turn test created, test it exists and has attributes

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,1 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/turn'
+require './lib/card'
 
+class TurnTest <  Minitest::Test
+  def test_it_exists_and_has_attributes
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Turn, turn
+    assert_equal card, turn.card
+  end
+
+end
